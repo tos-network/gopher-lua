9172d86391e8e04844976c3c7aa4e39ef131b818
Tomi Setsu <33028500+tomisetsu@users.noreply.github.com>
2026-03-01 15:29:10 +0000

    test: cover bare 'tol help' dispatcher path

--- cmd/tolang/subcommands_test.go
+++ cmd/tolang/subcommands_test.go
@@ -38,6 +38,9 @@ func TestDispatchSubcommandRouting(t *testing.T) {
 	if handled, code := dispatchSubcommand([]string{"version"}); !handled || code != 0 {
 		t.Fatalf("version should be handled with code 0, got handled=%v code=%d", handled, code)
 	}
+	if handled, code := dispatchSubcommand([]string{"help"}); !handled || code != 0 {
+		t.Fatalf("help should be handled with code 0, got handled=%v code=%d", handled, code)
+	}
 	if handled, code := dispatchSubcommand([]string{"help", "compile"}); !handled || code != 0 {
 		t.Fatalf("help compile should be handled with code 0, got handled=%v code=%d", handled, code)
 	}
